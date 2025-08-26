c8aa5c302596c83ed10e8e5cc7aad12aa0b51399
DESKTOP-C5T1MTL\user <dynarise.headquarters@gmail.com>
2025-08-26 11:03:09 +0900

    Personの設計

--- Person.cs
+++ Person.cs
@@ -8,6 +8,21 @@ namespace Class_Practice2
 {
     internal class Person
     {
+        //変数
+        string name; //キャラの名前
+        int level; //レベル
+        int hp; //体力
+        string info = "ただの村人"; //キャラ属性の特徴
 
+        //命令（メソッド）
+        //そのキャラの情報を表示するメソッド
+        public void InfoDisplay()
+        {
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("名前：" + name);
+            Console.WriteLine("レベル：" + level);
+            Console.WriteLine("体力：" + hp);
+            Console.WriteLine(info);
+        }
     }
 }
